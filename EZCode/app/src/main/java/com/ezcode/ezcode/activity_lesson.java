a05c0628d3737e33afa503c5a01847bd0be99963
Hai Nam <document.hnamdpt@gmail.com>
2019-03-31 00:46:06 +0700

    update lesson

--- EZCode/app/src/main/java/com/ezcode/ezcode/activity_lesson.java
+++ EZCode/app/src/main/java/com/ezcode/ezcode/activity_lesson.java
@@ -1,21 +1,30 @@
 package com.ezcode.ezcode;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.webkit.WebView;
 
-public class activity_lesson extends AppCompatActivity {
+public class activity_lesson extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
+    WebView webViewLesson;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
         Toolbar toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        webViewLesson = (WebView)findViewById(R.id.lesson_webview);
+        navigationView.setNavigationItemSelectedListener(this);
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
@@ -31,5 +40,29 @@ public class activity_lesson extends AppCompatActivity {
             super.onBackPressed();
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        switch (menuItem.getItemId()){
+//            case R.id.lesson_html_1:
+////                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+////                            new MessageFragment()).commit();
+//                webViewLesson.loadUrl("file:///android_asset/login.html");
+//                break;
+//            case R.id.nav_profile:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+////                        new ChatFragment()).commit();
+//                webViewLesson.loadUrl("file:///android_asset/news.html");
+//                break;
+//        }
+//        switch (getResources().getResourceEntryName(menuItem.getItemId())){
+//            case "":
+//                break;
+//        }
+        String lessonName = getResources().getResourceEntryName(menuItem.getItemId());
+        webViewLesson.loadUrl("file:///android_asset/"+lessonName+".html");
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
