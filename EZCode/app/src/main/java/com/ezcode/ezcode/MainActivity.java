25a6da60d4099131f1d07ea10b33039b4e6a21c8
Hai Nam <document.hnamdpt@gmail.com>
2019-03-30 21:48:12 +0700

    Nam cm

--- EZCode/app/src/main/java/com/ezcode/ezcode/MainActivity.java
+++ EZCode/app/src/main/java/com/ezcode/ezcode/MainActivity.java
@@ -1,17 +1,43 @@
 package com.ezcode.ezcode;
 
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView buttonNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        buttonNavigation = (BottomNavigationView)findViewById(R.id.navigation_butttom);
+        buttonNavigation.setOnNavigationItemSelectedListener(navListen);
+        buttonNavigation.setSelectedItemId(R.id.nav_home);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_main_content,new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListen = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+                case R.id.nav_message:
+                    selectedFragment = new MessageFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_main_content,selectedFragment).commit();
+            return true;
+        }
+    };
 }
--- EZCode/app/src/main/java/com/ezcode/ezcode/activity_login.java
+++ EZCode/app/src/main/java/com/ezcode/ezcode/activity_login.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class activity_login extends AppCompatActivity {
 
     RelativeLayout rellay1,rellay2;
+    TextView tvDescription;
     Button btnLogin;
     Handler handler = new Handler();
     Runnable runnable= new Runnable() {
@@ -18,17 +20,27 @@ public class activity_login extends AppCompatActivity {
         public void run() {
             rellay1.setVisibility(View.VISIBLE);
             rellay2.setVisibility(View.VISIBLE);
-//            btnLogin = (Button) findViewById(R.id.btn_login);
+//          btnLogin = (Button) findViewById(R.id.btn_login);
+            tvDescription.setVisibility(View.INVISIBLE);
         }
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         rellay1 = (RelativeLayout)findViewById(R.id.reallay1);
         rellay2 = (RelativeLayout)findViewById(R.id.reallay2);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        tvDescription = (TextView)findViewById(R.id.tv_description);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_login.this, MainActivity.class);
+                startActivity(intent);
 
+            }
+        });
         handler.postDelayed(runnable,2000);
     }
 }
